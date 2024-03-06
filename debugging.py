9ec70ec4ac3257804e7f094c8c4f72dca6a4ea55
Axel <eslbaum5@gmail.com>
2024-03-06 20:44:19 +0100

    debugging

---
+++ debugging.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# run in a IDE the file with debug mode on
+
+
+@app.get("/")
+def root():
+    a = "a"
+    b = "b" + a
+    return {"hello world": b}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
